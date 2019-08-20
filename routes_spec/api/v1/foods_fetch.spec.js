efee171ba5c728c5634b58741c4b324e70d95f65
davisc0801 <davisc1014@gmail.com>
2019-08-20 16:27:10 -0600

    Add test for contents of fetch all food endpoint

--- routes_spec/api/v1/foods_fetch.spec.js
+++ routes_spec/api/v1/foods_fetch.spec.js
@@ -9,6 +9,7 @@ describe('api', () => {
 
   beforeEach(() => {
     shell.exec('npx sequelize db:migrate');
+    shell.exec('npx sequelize db:seed:all');
   });
 
   afterEach(() => {
@@ -20,10 +21,23 @@ describe('api', () => {
   })
 
   describe('api v1 food fetch all path', () => {
-    test('it should respond to a get request', () => {
-    return request(app).get('/api/v1/foods')
+    test('It should respond to a GET request', () => {
+    return request(app).get("/api/v1/foods")
       .then(response => {
         expect(response.statusCode).toBe(200);
+      });
+    });
+
+    test('It should return several food objects', () => {
+    return request(app).get("/api/v1/foods")
+      .then(response => {
+        expect(response.body.length).toBe(7);
+        expect(Object.keys(response.body[0])).toContain("id");
+        expect(Object.keys(response.body[0])).toContain("name");
+        expect(Object.keys(response.body[0])).toContain("calories");
+        expect(Object.keys(response.body[6])).toContain("id");
+        expect(Object.keys(response.body[6])).toContain("name");
+        expect(Object.keys(response.body[6])).toContain("calories");
       });
     });
   });
