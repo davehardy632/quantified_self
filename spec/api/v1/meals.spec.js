3933697df739f6d5413a1c67589b91e5d751a35d
David Hardy <davehardy632@gmail.com>
2019-08-20 19:55:57 -0600

    Creating a test to return a 200 status and formatted json response when a user hits the endpoint with a valid meal id, also creating a sad path test to return a 404 status and message indicating the meal was not found when an invalid meal id is sent in the url.

--- spec/api/v1/meals.spec.js
+++ spec/api/v1/meals.spec.js
@@ -32,13 +32,40 @@ describe('api', () => {
   })
 
   describe ("test the get all meals endpoint", async () => {
-    test("should return a 201 status", async () => {
+    test("should return a 200 status and formatted meals response", async () => {
       return request(app).get("/api/v1/meals")
       .set('Accept', 'application/json')
       .then(response => {
         expect(response.statusCode).toBe(200);
         expect(Object.keys(response.body[0])).toEqual(["id", "name", "Food"]);
         expect(Object.keys(response.body[0]["Food"][0])).toEqual(["id", "name", "calories"]);
+        expect(Object.keys(response.body[0]["Food"][1])).toEqual(["id", "name", "calories"]);
+        expect(Object.keys(response.body[0]["Food"][2])).toEqual(["id", "name", "calories"]);
+      })
+    })
+  })
+
+  describe ("test the endpoint to return a meal and associated record by meal id", async () => {
+    test("should return a 200 status, and a formatted response for a single meal", async () => {
+      return request(app).get("/api/v1/meals/3/foods")
+      .set('Accept', 'application/json')
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+        expect(Object.keys(response.body[0])).toEqual(["id", "name", "Food"]);
+        expect(Object.keys(response.body[0]["Food"][0])).toEqual(["id", "name", "calories"]);
+        expect(Object.keys(response.body[0]["Food"][1])).toEqual(["id", "name", "calories"]);
+        expect(Object.keys(response.body[0]["Food"][2])).toEqual(["id", "name", "calories"]);
+      })
+    })
+  })
+
+  describe ("test meal id endpoint for nonexistent record", async () => {
+    test("should return a 200 status, response stating the record doesn't exist", async () => {
+      return request(app).get("/api/v1/meals/23/foods")
+      .set('Accept', 'application/json')
+      .then(response => {
+        expect(response.statusCode).toBe(404);
+        expect(response.body).toEqual("Meal not found");
       })
     })
   })
