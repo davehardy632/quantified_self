4fe9f8f5463a282259a180e55d65f5936de27ede
davisc0801 <davisc1014@gmail.com>
2019-08-21 12:07:46 -0600

    Remove unneeded async calls in tests and unused requirements

--- spec/api/v1/meals.spec.js
+++ spec/api/v1/meals.spec.js
@@ -6,8 +6,6 @@ var router = express.Router();
 var Meal = require('../../../models').Meal;
 var Food = require('../../../models').Food;
 var MealFoods = require('../../../models').MealFoods;
-const axios = require('axios');
-const keys = require("dotenv")
 
 
 describe('api', () => {
@@ -31,8 +29,8 @@ describe('api', () => {
     })
   })
 
-  describe ("test the get all meals endpoint", async () => {
-    test("should return a 200 status and formatted meals response", async () => {
+  describe ("test the get all meals endpoint", () => {
+    test("should return a 200 status and formatted meals response", () => {
       return request(app).get("/api/v1/meals")
       .set('Accept', 'application/json')
       .then(response => {
@@ -45,8 +43,8 @@ describe('api', () => {
     })
   })
 
-  describe ("test the endpoint to return a meal and associated record by meal id", async () => {
-    test("should return a 200 status, and a formatted response for a single meal", async () => {
+  describe ("test the endpoint to return a meal and associated record by meal id", () => {
+    test("should return a 200 status, and a formatted response for a single meal", () => {
       return request(app).get("/api/v1/meals/3/foods")
       .set('Accept', 'application/json')
       .then(response => {
@@ -61,8 +59,8 @@ describe('api', () => {
 
   ///// Sad path for retrieving an invalid meal
 
-  describe ("test meal id endpoint for nonexistent record", async () => {
-    test("should return a 404 status, response stating the record doesn't exist", async () => {
+  describe ("test meal id endpoint for nonexistent record", () => {
+    test("should return a 404 status, response stating the record doesn't exist", () => {
       return request(app).get("/api/v1/meals/23/foods")
       .set('Accept', 'application/json')
       .then(response => {
@@ -73,8 +71,8 @@ describe('api', () => {
   })
 
 
-  describe ("Adding a food to a meal endpoint", async () => {
-    test("should return a 200 status, stating the food has been added to the meal", async () => {
+  describe ("Adding a food to a meal endpoint", () => {
+    test("should return a 200 status, stating the food has been added to the meal", () => {
       return request(app).post("/api/v1/meals/1/foods/3")
       .set('Accept', 'application/json')
       .then(response => {
@@ -86,8 +84,8 @@ describe('api', () => {
 
   /////// sad path for creating association between a meal and food
 
-  describe ("Sad Path: Adding a food to a meal endpoint with invalid food id", async () => {
-    test("should return a 404 status, stating the food entry is invalid", async () => {
+  describe ("Sad Path: Adding a food to a meal endpoint with invalid food id", () => {
+    test("should return a 404 status, stating the food entry is invalid", () => {
       return request(app).post("/api/v1/meals/1/foods/32")
       .set('Accept', 'application/json')
       .then(response => {
@@ -97,8 +95,8 @@ describe('api', () => {
     })
   })
 
-  describe ("Sad Path: Adding a food to a meal endpoint with invalid meal id", async () => {
-    test("should return a 200 status, stating the meal entry is invalid", async () => {
+  describe ("Sad Path: Adding a food to a meal endpoint with invalid meal id", () => {
+    test("should return a 200 status, stating the meal entry is invalid", () => {
       return request(app).post("/api/v1/meals/13/foods/3")
       .set('Accept', 'application/json')
       .then(response => {
@@ -108,8 +106,8 @@ describe('api', () => {
     })
   })
 
-  describe ("Sad Path: Adding a food to a meal endpoint that already exists", async () => {
-    test("should return a 404 status, stating the association already exists", async () => {
+  describe ("Sad Path: Adding a food to a meal endpoint that already exists", () => {
+    test("should return a 404 status, stating the association already exists", () => {
       return request(app).post("/api/v1/meals/4/foods/7")
       .set('Accept', 'application/json')
       .then(response => {
@@ -119,8 +117,8 @@ describe('api', () => {
     })
   })
 
-  describe ("Sad Path: Adding a food to a meal endpoint where both ids are invalid", async () => {
-    test("should return a 404 status, stating the entry was invalid", async () => {
+  describe ("Sad Path: Adding a food to a meal endpoint where both ids are invalid", () => {
+    test("should return a 404 status, stating the entry was invalid", () => {
       return request(app).post("/api/v1/meals/42/foods/17")
       .set('Accept', 'application/json')
       .then(response => {
