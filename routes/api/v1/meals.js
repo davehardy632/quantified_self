80a73a4a7211d4989489d67b9433aa86da666fea
David Hardy <davehardy632@gmail.com>
2019-08-20 19:26:30 -0600

    Create an error message when a user requests a meal with an id that does not exist.

--- routes/api/v1/meals.js
+++ routes/api/v1/meals.js
@@ -12,22 +12,17 @@ router.get("/", async function(req, res, next) {
   res.status(200).send(JSON.stringify(meals));
 });
 
+
 router.get("/:meal_id/foods", async function(req, res, next) {
   mealId = parseInt(req.url.split("/")[1])
-
   meals = await mealsAndFoodsByMealId(mealId)
-
-  console.log(meals[0]["id"])
-
-  // Meal.findAll({ where: {id: mealId}, attributes: ["id", "name"], include: [{model: Food, attributes: ['id', 'name', 'calories'], through: {attributes: [] }}]  })
-  // .then(meals => {
-  //   res.setHeader("Content-Type", "application/json");
-  //   res.status(200).send(JSON.stringify(meals));
-  // })
-  // .catch(error => {
-  //   res.setHeader("Content-Type", "application/json");
-  //   res.status(404).send({ error });
-  // });
+  if (Object.keys(meals) == '0') {
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).send(JSON.stringify(meals));
+  } else {
+    res.setHeader("Content-Type", "application/json");
+    res.status(404).send(JSON.stringify("Meal not found"));
+  }
 });
 
 //////// async functions
