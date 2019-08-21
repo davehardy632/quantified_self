3d7c969f6cc12d3d0b69fb48899921465036ddea
davisc0801 <davisc1014@gmail.com>
2019-08-21 14:01:12 -0600

    Move contents of the food fetch spec to a single API spec

--- spec/api/v1/api.spec.js
+++ spec/api/v1/api.spec.js
@@ -10,16 +10,15 @@ var MealFoods = require('../../../models').MealFoods;
 
 describe('api', () => {
   beforeAll(() => {
-    shell.exec('npx sequelize db:create')
-  });
-  beforeEach(() => {
-      shell.exec('npx sequelize db:migrate')
-      shell.exec('npx sequelize db:seed:all')
-    });
-  afterEach(() => {
-    shell.exec('npx sequelize db:migrate:undo:all')
+    shell.exec('npx sequelize db:drop');
+    shell.exec('npx sequelize db:create');
+    shell.exec('npx sequelize db:migrate');
   });
 
+  beforeEach(() => {
+    
+    shell.exec('npx sequelize db:seed:all');
+  });
 
   describe ("test the root path", () => {
     test("should return a 202 status", () => {
@@ -127,4 +126,26 @@ describe('api', () => {
       })
     })
   })
+
+  describe('api v1 food fetch all path', () => {
+    test('It should respond to a GET request', () => {
+    return request(app).get("/api/v1/foods")
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+      });
+    });
+
+    test('It should return several food objects', () => {
+    return request(app).get("/api/v1/foods")
+      .then(response => {
+        expect(response.body.length).toBe(7);
+        expect(Object.keys(response.body[0])).toContain("id");
+        expect(Object.keys(response.body[0])).toContain("name");
+        expect(Object.keys(response.body[0])).toContain("calories");
+        expect(Object.keys(response.body[6])).toContain("id");
+        expect(Object.keys(response.body[6])).toContain("name");
+        expect(Object.keys(response.body[6])).toContain("calories");
+      });
+    });
+  });
 })
