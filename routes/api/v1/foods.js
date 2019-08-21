7614f00613677cea0c79e388e31ee1a47134cf3d
davisc0801 <davisc1014@gmail.com>
2019-08-21 15:42:59 -0600

    Add route to fetch a single food item

--- routes/api/v1/foods.js
+++ routes/api/v1/foods.js
@@ -2,6 +2,28 @@ const express = require('express');
 const router = express.Router();
 const Food = require('../../../models').Food;
 
+router.get('/:id', function(req, res) {
+  Food.findOne(
+    {
+      attributes: ["id", "name", "calories"],
+      where: { id: req.params.id }
+    }
+  )
+  .then(food => {
+    if (food) {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).send(JSON.stringify(food));
+    } else {
+      res.setHeader("Content-Type", "application/json");
+      res.status(404).send(JSON.stringify({error: "No food with that ID can be found."}));
+    }
+  })
+  .catch(error => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(500).send(JSON.stringify(error));
+  });
+});
+
 router.get('/', function(req, res) {
   Food.findAll({attributes: ["id", "name", "calories"]})
   .then(foods => {
