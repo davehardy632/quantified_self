55ee91346a9ee77e02883587b98cea5bc16ca2df
davisc0801 <davisc1014@gmail.com>
2019-08-21 14:00:41 -0600

    Change database info to use environment variables

--- config/config.js
+++ config/config.js
@@ -1,24 +1,26 @@
-{
+require('dotenv').config();
+
+module.exports = {
   "development": {
-    "username": "postgres",
+    "username": process.env.DB_USER,
     "password": null,
-    "database": "travis_ci_test",
+    "database": process.env.DB_NAME_DEV,
     "host": "127.0.0.1",
     "dialect": "postgres",
     "operatorsAliases": false
   },
   "test": {
-    "username": "postgres",
+    "username": process.env.DB_USER,
     "password": null,
-    "database": "travis_ci_test",
+    "database": process.env.DB_NAME_TEST,
     "host": "127.0.0.1",
     "dialect": "postgres",
     "operatorsAliases": false
   },
   "production": {
-    "username": "postgres",
+    "username": process.env.DB_USER,
     "password": null,
-    "database": "travis_ci_test",
+    "database": process.env.DB_NAME_PROD,
     "host": "127.0.0.1",
     "dialect": "postgres",
     "operatorsAliases": false
