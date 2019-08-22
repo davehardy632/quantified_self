22db8947e18a10c07ecd9a0ef0b67a825bec2cd8
davisc0801 <davisc1014@gmail.com>
2019-08-22 12:07:04 -0600

    Add route to create a new food object in the database

--- routes/api/v1/foods.js
+++ routes/api/v1/foods.js
@@ -36,4 +36,36 @@ router.get('/', function(req, res) {
   });
 });
 
+router.post('/', function(req, res) {
+  console.log(req.body.food.name)
+  if (req.body.food){
+    Food.findOne(
+      {
+        attributes: ["id", "name", "calories"],
+        where: { name: req.body.food.name }
+      }
+    ).then(food => {
+      if (!food) {
+        Food.create({
+          name: req.body.food.name,
+          calories: req.body.food.calories
+        }).then(newFood => {
+          res.setHeader("Content-Type", "application/json");
+          res.status(201).send(JSON.stringify(newFood));
+        })
+      } else {
+        res.setHeader("Content-Type", "application/json");
+        res.status(400).send(JSON.stringify({error: "Food already exists."}));
+      }
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send(JSON.stringify(error));
+    });
+  } else {
+    res.setHeader("Content-Type", "application/json");
+    res.status(406).send(JSON.stringify({error: "Invalid Entry."}));
+  }
+});
+
 module.exports = router;
